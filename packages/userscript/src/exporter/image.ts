fefda8245a8d5f8750ca06ccbda4797d72dccf68
Pionxzh <spigbbbbb@gmail.com>
2023-02-27 14:18:48 +0800

    fix: try fix #53 with specific scale in html2canvas

--- packages/userscript/src/exporter/image.ts
+++ packages/userscript/src/exporter/image.ts
@@ -2,6 +2,11 @@ import html2canvas from 'html2canvas'
 import { downloadUrl, getFileNameWithFormat } from '../utils/download'
 import { sleep } from '../utils/utils'
 
+// https://github.com/niklasvh/html2canvas/issues/2792#issuecomment-1042948572
+function fnIgnoreElements(el: any) {
+    return typeof el.shadowRoot === 'object' && el.shadowRoot !== null
+}
+
 export async function exportToPng(fileNameFormat: string) {
     const thread = document.querySelector('main .group')?.parentElement as HTMLElement
     if (!thread || thread.children.length === 0) return
@@ -12,10 +17,12 @@ export async function exportToPng(fileNameFormat: string) {
     await sleep(100)
 
     const canvas = await html2canvas(thread, {
+        scale: 1,
         scrollX: -window.scrollX,
         scrollY: -window.scrollY,
         windowWidth: thread.scrollWidth,
         windowHeight: thread.scrollHeight,
+        ignoreElements: fnIgnoreElements,
     })
 
     // restore the layout
@@ -25,4 +32,5 @@ export async function exportToPng(fileNameFormat: string) {
         .replace(/^data:image\/[^;]/, 'data:application/octet-stream')
     const fileName = getFileNameWithFormat(fileNameFormat, 'png')
     downloadUrl(fileName, dataUrl)
+    window.URL.revokeObjectURL(dataUrl)
 }
