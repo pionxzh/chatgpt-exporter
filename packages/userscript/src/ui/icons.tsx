659a7f8fd59125787d6f4c108c25a634012adb03
Pionxzh <spigbbbbb@gmail.com>
2023-03-05 18:35:12 +0800

    feat: add loading effect on menu button

--- packages/userscript/src/ui/icons.tsx
+++ packages/userscript/src/ui/icons.tsx
@@ -22,3 +22,28 @@ export function IconCopy() {
 export function IconArrowRightFromBracket() {
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-4 h-4" fill="currentColor">{/* <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}<path d="M534.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L434.7 224 224 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM192 96c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-53 0-96 43-96 96l0 256c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" /></svg>
 }
+
+export function IconLoading() {
+    return (
+        <span style={{ animation: '1.4s linear 0s infinite normal none running rotate' }}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="22 22 44 44"
+                className="w-4 h-4"
+                style={{ animation: '1.4s ease-in-out 0s infinite normal none running circularDash' }}
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <circle
+                    cx="44"
+                    cy="44"
+                    r="20.2"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="3.6">
+                </circle>
+            </svg>
+        </span>
+    )
+}
