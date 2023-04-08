dcd9c970adefa6aaea699148b62b49274218e31a
Pionxzh <spigbbbbb@gmail.com>
2023-04-08 13:35:07 +0800

    fix: fix toggle style in mobile view

--- packages/userscript/src/ui/Toggle.tsx
+++ packages/userscript/src/ui/Toggle.tsx
@@ -18,7 +18,7 @@ export const Toggle = (props: ToggleProps) => {
                 onChange={handleChange}
                 className="sr-only peer"
             />
-            <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <div className="relative shrink-0 w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 empty:hidden">{label}</span>
         </label>
     )
