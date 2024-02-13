efbd662c772c6e6188b93c166397f1a98cb9baaf
Pionxzh <pionxzh@csie.io>
2024-02-13 17:05:41 +0800

    feat: support /gpts paths

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,9 @@ export default defineConfig({
                     'https://chat.openai.com/c/*',
                     // support https://chat.openai.com/g/g-123456789
                     'https://chat.openai.com/g/*',
+                    // support https://chat.openai.com/gpts/
+                    'https://chat.openai.com/gpts',
+                    'https://chat.openai.com/gpts/*',
                     // support https://chat.openai.com/share/123456789
                     'https://chat.openai.com/share/*',
                     // support https://chat.openai.com/share/123456789/continue
